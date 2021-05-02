fe359c450b64847c9931c8ace026997e5c117808
David Rios <mr.daverios@gmail.com>
2021-05-02 19:56:29 -0400

    Ready for release

---
+++ index.js
@@ -0,0 +1,62 @@
+const LATEST_VERSION = '1.4.6';
+
+const downloadButton = document.getElementById('downloadButton');
+
+let link = 'https://github.com/MrDavidRios/remindr_releases/releases';
+
+initDownloadButton();
+
+function initDownloadButton() {
+	const os = getOS();
+
+	switch (os) {
+		case 'Windows':
+			link = `https://github.com/MrDavidRios/remindr_releases/releases/download/v${LATEST_VERSION}/Remindr-Setup-${LATEST_VERSION}.exe`;
+
+			downloadButton.innerText = 'Download for Windows';
+			break;
+		case 'Linux':
+			link = `https://github.com/MrDavidRios/remindr_releases/releases/download/v${LATEST_VERSION}/Remindr-${LATEST_VERSION}.dmg`;
+
+			downloadButton.innerText = 'Download for Linux';
+			break;
+		case 'Mac OS':
+			link = `https://github.com/MrDavidRios/remindr_releases/releases/download/v${LATEST_VERSION}/Remindr-${LATEST_VERSION}-mac.zip`;
+
+			downloadButton.innerText = 'Download for Mac OS';
+			break;
+		default:
+			break;
+	}
+}
+
+downloadButton.addEventListener('click', () => {
+	window.open(link, '_blank');
+});
+
+document.getElementById('mobileReleasesButton').addEventListener('click', () => {
+	window.open('https://github.com/MrDavidRios/remindr_releases/releases', '_blank');
+});
+
+function getOS() {
+	var userAgent = window.navigator.userAgent,
+		platform = window.navigator.platform,
+		macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+		windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+		iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+		os = null;
+
+	if (macosPlatforms.indexOf(platform) !== -1) {
+		os = 'Mac OS';
+	} else if (iosPlatforms.indexOf(platform) !== -1) {
+		os = 'iOS';
+	} else if (windowsPlatforms.indexOf(platform) !== -1) {
+		os = 'Windows';
+	} else if (/Android/.test(userAgent)) {
+		os = 'Android';
+	} else if (!os && /Linux/.test(platform)) {
+		os = 'Linux';
+	}
+
+	return os;
+}
