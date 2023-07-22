a565a4fb4cee3d724655a0fac435497463760460
David Rios <52746497+MrDavidRios@users.noreply.github.com>
2023-07-22 16:49:40 -0700

    feat: scroll animations improved, blur compatibility improved

--- scrollanimations.js
+++ scrollanimations.js
@@ -1,15 +1,20 @@
 const observer = new IntersectionObserver(
-	(entries) => {
-		entries.forEach((entry) => {
-			if (entry.isIntersecting) {
-				if (entry.target.getAttribute('direction') == 'left') entry.target.classList.add('slide-in-from-left');
-				else entry.target.classList.add('slide-in-from-right');
-			}
-		});
-	},
-	{ threshold: 0.8 }
+  (entries) => {
+    entries.forEach((entry) => {
+      const direction = entry.target.getAttribute("direction");
+
+      if (entry.isIntersecting) {
+        entry.target.classList.remove(`slide-out-from-${direction}`);
+        entry.target.classList.add(`slide-in-from-${direction}`);
+      } else {
+        entry.target.classList.add(`slide-out-from-${direction}`);
+        entry.target.classList.remove(`slide-in-from-${direction}`);
+      }
+    });
+  },
+  { threshold: 0.4 }
 );
 
-document.querySelectorAll('.project-showcase').forEach((el) => {
-	observer.observe(el);
+document.querySelectorAll(".project-showcase").forEach((el) => {
+  observer.observe(el);
 });
